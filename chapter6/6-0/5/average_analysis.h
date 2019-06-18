20864eef298f9f4591d9175a6d0845a05653a496
Aidan Prehatny <aprehatny@yahoo.com>
2019-06-18 02:00:36 -0700

    add grade analysis single pass algo

---
+++ chapter6/6-0/5/average_analysis.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_AVERAGE_ANALYSIS_H
+#define GUARD_AVERAGE_ANALYSIS_H
+ 
+// average_analysis.h
+ 
+#include <vector>
+#include "Student_info.h"
+ 
+double average_analysis(const std::vector<Student_info>&);
+ 
+#endif // GUARD_AVERAGE_ANALYSIS_H
