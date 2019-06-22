868607c33e22511f6896e39eb250a3a2cc1c4d4f
Aidan Prehatny <aprehatny@yahoo.com>
2019-06-21 17:42:27 -0700

    add copy iterator template func using back insert

--- chapter8/8-2/main.cpp
+++ chapter8/8-2/main.cpp
@@ -9,14 +9,34 @@ using namespace std;
 
 template<class In1, class In2>
 bool equalMine(In1 first, In1 last, In2 policy) {
-    while (first != last) {
+    while (first != prev(last) || !(*first == *policy)) {
+        cout << *first;
         if (!(*first == *policy)) {
             return false;
         }
-        first++; policy++;
+         first++; policy++;
     }
     return true;
-} 
+}
+
+template<class It, class ToFind>
+bool findMine(It first, It const last, ToFind const &toFind) {
+    while (first != last || *first == toFind) {
+        if (*first == toFind) {
+            return true;
+        }
+        first++;
+    }
+    return false;
+}
+
+template<class It, class CopyTo>
+CopyTo copyMine(It first, It const last, CopyTo copyTo) {
+    while (first != last) {
+        *copyTo++ = *first++;
+    }
+    return copyTo;
+}
 
 int main() {
 
@@ -31,14 +51,24 @@ int main() {
                     [&](auto word) {words.push_back(word); });
     };
     vector<string>::iterator first = words.begin();
-    vector<string>::iterator last = prev(words.end());
+    vector<string>::iterator last = words.end();
+    
+    // for equal
     vector<string>::iterator last1 = range.begin();
+    // for find
+    // string last1 = "gucci";
 
-    if (equalMine(first, last, last1)) {
-        cout << "found a match" << endl;
-    } else {
-        cout << "no match found" << endl;
+    // for copy
+    copyMine(first, last, back_inserter(range));
+    for (auto const &word : range) {
+        cout << word << endl;
     }
+
+    // if (equalMine(first, last, last1)) {
+    //     cout << "found a match" << endl;
+    // } else {
+    //     cout << "no match found" << endl;
+    // }
 
     return 0;
 }
