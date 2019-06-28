ade8feaedfa8de711ad015d9278d2e2807da8060
Aidan Prehatny <aprehatny@yahoo.com>
2019-06-28 08:03:01 -0700

    add chapter10 brief

---
+++ chapter10/10-0/main.cpp
@@ -0,0 +1,6 @@
+// 10-1 rewrite the student grading program from 9.6/166 to generate letter grades
+// 10-2 rewrite the median function from 8.1.1/140 so that we can call it wit either a vector or a build in array. the function should allow containers of any arithmetic type
+// 10-3 write a test program to verify that the median function operates correctly. Ensure that calling median does not change the order of the elements in the container.
+// 10-4 write a class that implements a list that holds strings
+// 10-5 write a bidirectional iterator for your String_list class
+// 10-6 test the class by rewriting the split function to put its output into a String_list
