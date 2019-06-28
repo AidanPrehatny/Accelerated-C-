89e506c96d51b25f1122e90a86072dd62264bc07
Aidan Prehatny <aprehatny@yahoo.com>
2019-06-28 11:21:41 -0700

    add template class for median of containers + array

--- chapter10/10-0/main.cpp
+++ chapter10/10-0/main.cpp
@@ -4,3 +4,59 @@
 // 10-4 write a class that implements a list that holds strings
 // 10-5 write a bidirectional iterator for your String_list class
 // 10-6 test the class by rewriting the split function to put its output into a String_list
+
+#include <stdio.h>
+#include <cstring>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+char* duplicate_chars(const char* p) {
+    size_t length = strlen(p) + 1;
+    char* result = new char[length];
+
+    std::copy(p, p + length, result);
+    return result;
+}
+
+int* pointer_to_dynamic() {
+    return new int(0);
+}
+
+std::string letter_grade(double grade) {
+    static const double numbers[] = {
+        97, 94, 90, 87, 84, 80, 77, 74, 70, 60, 0
+    };
+    static const char* const letters[] = {
+        "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "F"
+    };
+
+    static const size_t ngrades = sizeof(numbers)/sizeof(*numbers);
+    for (size_t i = 0; i < ngrades; ++i) {
+        if (grade >= numbers[i]) {
+            return letters[i];
+        }
+    }
+    return "?\?\?";
+}
+
+int main(int argc, char ** argv) {
+    int fail_count =0;
+
+    for (int i = 1; i < argc; ++i) {
+        std::ifstream in(argv[i]);
+        if (in) {
+            std::string s;
+            while (getline(in, s)) {
+                std::cout << s << std::endl;
+            }
+        } else {
+            std::cerr << "cannot open file " << argv[i] << std::endl;
+            ++fail_count;
+        }
+    }
+    std::string grade = letter_grade(85.0);
+    std::cout << grade << std::endl;
+    return fail_count;
+}
---
+++ chapter10/10-2/main.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
+
+using std::domain_error;
+using std::sort;
+using std::vector;
+
+template <class T, class Iterator>
+T median(Iterator begin, Iterator end) {
+  if (begin == end)
+    throw domain_error("median of an empty container");
+  
+  vector<T> temp;
+  
+  for (auto &b; end)
+    temp.push_back(b);
+
+  sort(temp.begin(), temp.end());
+
+  size_t mid = temp.size() / 2;
+
+  T ret = (temp.size() % 2 == 0)
+    ? (temp[mid] + temp[mid - 1]) / 2
+    : temp[mid];
+
+  return ret;
+}
